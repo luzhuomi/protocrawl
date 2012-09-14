504918b095fab63b7d148c5250cfb5e3e10044eb
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2012-09-14 10:20:09 +0800

    added retry

--- twitter/trunk/stream/stream.py
+++ twitter/trunk/stream/stream.py
@@ -188,11 +188,21 @@ if len(sys.argv) < 1:
 
 user_ids = get_userids_file(sys.argv[1])
 
-conn = pycurl.Curl()
-conn.setopt(pycurl.USERPWD, "%s:%s" % (USER, PASS))
-conn.setopt(pycurl.URL, STREAM_URL+','.join(map(str,user_ids)))
-conn.setopt(pycurl.WRITEFUNCTION, on_receive)
-conn.perform()
+def loop(retry):
+    try:
+        conn = pycurl.Curl()
+        conn.setopt(pycurl.USERPWD, "%s:%s" % (USER, PASS))
+        conn.setopt(pycurl.URL, STREAM_URL+','.join(map(str,user_ids)))
+        conn.setopt(pycurl.WRITEFUNCTION, on_receive)
+        conn.perform()
+    except:
+        if retry > 0:
+            loop(retry-1)
+        else:
+            sys.exit(0)
+
+loop(5)
+    
 
 s1 = '{"in_reply_to_status_id_str":null,"contributors":null,"place":null,"in_reply_to_screen_name":null,"text":"Test","favorited":false,"in_reply_to_user_id_str":null,"coordinates":null,"geo":null,"retweet_count":0,"created_at":"Fri Jul 13 09:38:00 +0000 2012","source":"web","in_reply_to_user_id":null,"in_reply_to_status_id":null,"retweeted":false,"id_str":"223712876576260096","truncated":false,"user":{"favourites_count":10,"friends_count":42,"profile_background_color":"C6E2EE","following":null,"profile_background_tile":false,"profile_background_image_url_https":"https:\/\/si0.twimg.com\/images\/themes\/theme2\/bg.gif","followers_count":38,"profile_image_url":"http:\/\/a0.twimg.com\/profile_images\/60558692\/KennyMugShot_normal.jpg","contributors_enabled":false,"geo_enabled":false,"created_at":"Tue Sep 23 03:08:29 +0000 2008","profile_sidebar_fill_color":"DAECF4","description":"Circos.com, Computer Science, Phd, National University of Singapore","listed_count":1,"follow_request_sent":null,"time_zone":"Singapore","url":"http:\/\/sites.google.com\/site\/luzhuomi\/","verified":false,"profile_sidebar_border_color":"C6E2EE","default_profile":false,"show_all_inline_media":false,"is_translator":false,"notifications":null,"profile_use_background_image":true,"protected":false,"profile_image_url_https":"https:\/\/si0.twimg.com\/profile_images\/60558692\/KennyMugShot_normal.jpg","location":"Singapore","id_str":"16414559","profile_text_color":"663B12","name":"Kenny","statuses_count":199,"profile_background_image_url":"http:\/\/a0.twimg.com\/images\/themes\/theme2\/bg.gif","id":16414559,"default_profile_image":false,"lang":"en","utc_offset":28800,"profile_link_color":"1F98C7","screen_name":"luzm"},"id":223712876576260096,"entities":{"user_mentions":[],"urls":[],"hashtags":[]}}'
 
