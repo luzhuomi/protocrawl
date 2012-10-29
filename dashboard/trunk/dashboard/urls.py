41d030d6973cfda6f8b2ca68db7e842214bbb307
Kenny Lu <luzhuomi@gmail.com>
2012-10-29 22:52:07 +0800

    shifted the module

--- dashboard/trunk/dashboard/urls.py
+++ dashboard/trunk/dashboard/urls.py
@@ -7,14 +7,14 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-        (r'^$', 'main.views.home'),
-        (r'', include('gmapi.urls.media')),        
-        (r'tweet/(?P<page_index>\d+)/(?P<page_size>\d+)/', 'main.views.tweet_ajax'),
-        (r'tweet/total/(?P<page_size>\d+)/', 'main.views.tweet_total_ajax'),        
-        (r'mood/(?P<age_band>.+)/(?P<gender>.+)/(?P<race>.+)/(?P<mood>.+)/', 'main.views.mood_ajax'),
-        (r'profile/(?P<age_band>.+)/(?P<gender>.+)/(?P<race>.+)/(?P<mood>.+)/(?P<dimension>.+)/', 'main.views.profile_ajax'),                
-        (r'^map/$', 'main.views.map'),
-        (r'^map/', include('gmapi.urls.media')), # for debug
+        (r'^main/$', 'main.views.home'),
+        (r'main/', include('gmapi.urls.media')),        
+        (r'main/tweet/(?P<page_index>\d+)/(?P<page_size>\d+)/', 'main.views.tweet_ajax'),
+        (r'main/tweet/total/(?P<page_size>\d+)/', 'main.views.tweet_total_ajax'),        
+        (r'main/mood/(?P<age_band>.+)/(?P<gender>.+)/(?P<race>.+)/(?P<mood>.+)/', 'main.views.mood_ajax'),
+        (r'main/profile/(?P<age_band>.+)/(?P<gender>.+)/(?P<race>.+)/(?P<mood>.+)/(?P<dimension>.+)/', 'main.views.profile_ajax'),                
+        (r'main/map/$', 'main.views.map'),
+        (r'main/map/', include('gmapi.urls.media')), # for debug
         (r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.APP_DIR, 'static_media/')}),
     # Examples:
     # url(r'^$', 'dashboard.views.home', name='home'),
